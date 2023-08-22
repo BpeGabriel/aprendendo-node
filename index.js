09ae8469c5041fed9cf2272f3e4319e003aa6a15
BpeGabriel <bpgabriel611@gmail.com>
2023-08-22 15:54:32 -0300

    adicionando uma biblioteca, o chalck

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('olá mundo'));
+
+
 console.log('olá mundo');
+console.log('São geralmente recuperados a partir de um objeto');
+console.log('seleção, pelo usuário, de arquivos através do elemento ');
+
