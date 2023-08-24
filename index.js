b95621cc290ba5b53b3da488d932ee8298592bb7
BpeGabriel <bpgabriel611@gmail.com>
2023-08-24 14:31:36 -0300

    Aplicando biblioteca FS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('olá mundo'));
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+        console.log(chalk.green(texto))
+    })
+}
 
-
-console.log('olá mundo');
-console.log('São geralmente recuperados a partir de um objeto');
-console.log('seleção, pelo usuário, de arquivos através do elemento ');
-
+pegaArquivo('./arquivos/texto.md');
